c6a189551e663f8eda6ab3ce2b21189dcdc1ef21
Gabriel <gabrielbarretos2000@outlook.com>
2022-02-26 23:59:41 -0300

    change font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -34,7 +34,16 @@ export const GlobalStyle = createGlobalStyle`
     background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
-  
+
+  body, input, text-area, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
