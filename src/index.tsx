dd97b322882bef5d8fc6f8863600ce424f30650d
Gabriel <gabrielbarretos2000@outlook.com>
2022-02-27 00:49:50 -0300

    add axios and miragejs for mocking

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { createServer } from "miragejs";
+
 import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de website",
+          amount: 12000,
+          type: "deposit",
+          category: "Desenvolvimento",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
